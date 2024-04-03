f26baa0c61a4363d8f0376b3c024c1b3870741be
Bellamy509 <81392220+Bellamy509@users.noreply.github.com>
2024-04-03 12:33:35 -0400

    Update DefaultResumeData.jsx

--- components/utility/DefaultResumeData.jsx
+++ components/utility/DefaultResumeData.jsx
@@ -1,55 +1,77 @@
 const DefaultResumeData = {
-  name: "MARCUS HALL",
-  position: "Developer",
-  contactInformation: "+1-555-0100",
-  email: "beddylea@gmail.com",
-  address: "San Francisco, CA",
+  name: "WIDJ BELLA",
+  position: "Founder & CO-CEO OF Smartschool",
+  contactInformation: "+5094932-4932",
+  email: "bellawidj@thesmartschool.tech",
+  address: "Cap-Haitien, HT",
   profilePicture: "",
   socialMedia: [
     {
       socialMedia: "Github",
-      link: "github.com//bedivere-lea",
+      link: "github.com//Bellamy509",
     },
     {
       socialMedia: "LinkedIn",
-      link: "linkedin.com/in/bedivere-lea",
+      link: "linkedin.com/in/widj-bella-81577825b/",
     },
     {
       socialMedia: "Website",
-      link: "bedivere-lea.github.io",
+      link: "widjbella.tech",
     },
   ],
-  summary: "Resourceful Developer with 11 years of experience in designing and developing user interfaces, testing and training employees. Skilled at utilizing a wide variety of tools and programs to provide effective applications.",
+  summary: " "At 21, I am a passionate about artificial intelligence committed to harnessing its potential for the good of humanity. As the founder and co-CEO of Smartschool, I strive to create innovative solutions that shape the future of education. A graduate of Harvard and Laval, with a distinction as the Best Computer Science Student in France 2023 by EDITx, I combine my skills in software engineering and machine learning to solve complex problems. In parallel, I am actively involved in climate defense, aware of the importance of preserving our planet for future generations. With a vision focused on ethics and progress, I am determined to shape a future where AI works in harmony with humanity.".",
   education: [
     {
       "school": "Bachelor of Computer Science",
-      "degree": "New York University",
-      "startYear": "2020-08-20",
-      "endYear": "2024-07-01"
+      "degree": "Havard University",
+      "startYear": "2019-08-20",
+      "endYear": "2023-07-01"
+    },
+  {
+      "school": "Bachelor of Computer Science",
+      "degree": "Havard University",
+      "startYear": "2019-08-20",
+      "endYear": "2023-07-01"
     },
   ],
   workExperience: [
     {
-      "company": "Torph TTC",
+      "company": "Defrilex contact center",
       "position": "Developer",
-      "description": "Torph TTC is a global software company that offers user interface UI development tools and components for a range of developer applications across all platforms.",
-      "keyAchievements": "Created and maintained 10 web applications for numerous national and foreign clients.\nEnsured that the user interfaces and user experience of the software applications developed by the team met at least 80% of users expectations.\nCreated and analyzed 500 unit test cases.\nDeveloped python scripts to automate image's noise-reduction process which helped improve research analysis time by 40%.\nEstablished and lead a team of 10 people; covering every key role in the early stages.",
-      "startYear": "2023-02-16",
-      "endYear": "2023-02-21"
+      "description": "As a software engineer at Defrilex Contact Center, I played a vital role in developing and maintaining innovative software solutions to optimize call center operations",
+      "keyAchievements": " My responsibilities included designing, developing, and deploying robust and high-performance applications in close collaboration with technical and operational teams. \nLeveraging my programming expertise and problem-solving skills, I contributed to enhancing internal processes' efficiency, automating repetitive tasks, and ensuring a smooth and satisfactory user experience for agents and clients.\nMy commitment to quality and innovation was a key factor in the success of projects aimed at modernizing and improving call center performance.",
+      "startYear": "2021-09-16",
+      "endYear": "2022-12-21"
+    },
+   {
+      "company": "ImjustWatch",
+      "position": "Founder & CO-CEO",
+      "description": "ImjustWatch is an on-demand streaming platform for movies and series, offering content from over 6 major streaming platforms under a single subscription.",
+      "keyAchievements": " Providing a centralized and convenient experience, ImjustWatch allows users to discover and watch a wide range of content from various sources, all from one interface. \nWith a vast selection of movies and series available, ImjustWatch streamlines the search and viewing process, giving subscribers instant access to their favorite entertainment, wherever they are.",
+      "startYear": "2022-12-16",
+      "endYear": "2023-07-21"
     },
     {
-      "company": "Reilty Group",
-      "position": "Front End Web Developer",
-      "description": "Reilty Group is an industry-leading provider of online gambling software and solutions.",
-      "keyAchievements": "Increased by 35% the reach of users to the platform, over the installation of the web platform in mobile devices.\nDelivered 30 web solutions.\nReplaced 1 full-time web developer under part-time employment achieving a productivity gain of over 60% as measured by working hours.\nWorked with more than 4 different teams at different points of time and provided end-to-end solutions for clients.",
-      "startYear": "2023-02-22",
-      "endYear": "2023-03-09"
+      "company": "Jybe Branding Agency",
+      "position": "Developer",
+      "description": "Jybe Branding Agency is an innovative company focused on creating impactful branding solutions.",
+      "keyAchievements": "As a developer at Jybe, I contributed to crafting compelling digital experiences and user interfaces for our clients. Collaborating closely with design and marketing teams, I played a key role in translating creative concepts into functional and visually appealing websites and applications. \nDelivered 30 web solutions.\n My responsibilities included coding, testing, and deploying responsive and intuitive digital solutions tailored to each client's unique branding goals.\norking at Jybe, I thrived in a dynamic environment where creativity and technical expertise intersected to deliver exceptional results for our clients.",
+      "startYear": "2021-02-22",
+      "endYear": ""
     },
     {
-      "company": "HellX",
-      "position": "Python Developer",
-      "description": "Heller is a Software Editor specialized in logistics management.",
-      "keyAchievements": "Developed several automation systems - saved over $50,000 annually.\nJoined the team as #10 employee as a developer working directly with the biggest customers.\nInvolved in coding as per the requirement, delivered well ahead of time which saved project cost by 20%.\nReduced database size by nearly 50% and improved its performance by 20%.\nDelivery time of good reduced by 50%.",
+      "company": "BigParyaj",
+      "position": "Head Developer",
+      "description": "At BigParyaj, a leading sports betting website, I served as the Head Developer, overseeing the technical aspects of our platform's development.",
+      "keyAchievements": "In this role, I led a team of developers in designing and implementing robust features to enhance the user experience and ensure the reliability and security of our betting platform.\nWorking closely with product managers and designers, I played a pivotal role in translating business requirements into technical specifications and guiding the team through the development lifecycle.\nMy responsibilities included architecting scalable solutions, conducting code reviews, and implementing best practices to optimize performance and maintainability. \nThrough effective leadership and collaboration, I contributed to the continuous improvement and success of BigParyaj as a premier destination for sports betting enthusiasts.",
+      "startYear": "2022-02-22",
+      "endYear": ""
+    }
+  {
+      "company": "Smartschool",
+      "position": "Founder & Co-CEO",
+      "description": "At BigParyaj, a leading sports betting website, I served as the Head Developer, overseeing the technical aspects of our platform's development.",
+      "keyAchievements": "In this role, I led a team of developers in designing and implementing robust features to enhance the user experience and ensure the reliability and security of our betting platform.\nWorking closely with product managers and designers, I played a pivotal role in translating business requirements into technical specifications and guiding the team through the development lifecycle.\nMy responsibilities included architecting scalable solutions, conducting code reviews, and implementing best practices to optimize performance and maintainability. \nThrough effective leadership and collaboration, I contributed to the continuous improvement and success of BigParyaj as a premier destination for sports betting enthusiasts.",
       "startYear": "2023-02-22",
       "endYear": "2023-03-09"
     }
